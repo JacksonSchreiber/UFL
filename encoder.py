1838df2a5f3c118d9ccb78835e3269335e3ea977
tcutajar <tcutajar@ufl.edu>
2023-10-23 23:14:45 -0400

    Added decode function to encoder.py and applied it into the main.

--- encoder.py
+++ encoder.py
@@ -1,8 +1,8 @@
-#Jackson Schreiber
+# Jackson Schreiber
 
 def encode(password):
     encoded_pass = ""
-    for i in password: #iterate through each password digit
+    for i in password:  # iterate through each password digit
         if int(i) < 7:
             encoded_pass += str(int(i) + 3)
         else:
@@ -10,33 +10,42 @@ def encode(password):
 
     return encoded_pass
 
-def decode(password):
-    return "test"
+
+def decode(passwrd):
+    temp_pswrd = ""
+    for i in range(0, len(passwrd)):
+        if int(passwrd[i]) <= 2:
+            temp_pswrd += str((int(passwrd[i]) - 3) + 10)
+        else:
+            temp_pswrd += str(int(passwrd[i]) - 3)
+    return temp_pswrd
+
 
 def main():
-    while(True):
+    while (True):
         print("Menu\n"
               "-------------\n"
               "1. Encode\n"
               "2. Decode\n"
               "3. Quit\n")
 
+        choice = input("Please enter an option: ")  # Menu selection stored
 
-        choice = input("Please enter an option: ") #Menu selection stored
-
-        if choice == "1": #Encoder function
+        if choice == "1":  # Encoder function
             plain_pass = input("Please enter your password to encode: ")
-            if len(plain_pass) != 8: #Check if password is 8 digits
+            if len(plain_pass) != 8:  # Check if password is 8 digits
                 print("The password must be 8 digits.")
                 plain_pass = ""
             else:
-                encoded_pass = encode(plain_pass) #Encode the password, store it in encoded_pass variable
-                plain_pass = "" #impenetrable 1337 security
+                encoded_pass = encode(plain_pass)  # Encode the password, store it in encoded_pass variable
+                plain_pass = ""  # impenetrable 1337 security
                 print("Your password has been encoded and stored!")
 
-        elif choice == "2": #Uses decoder function
+        elif choice == "2":  # Uses decoder function
             print(f'The encoded password is {encoded_pass}, and the original password is {decode(encoded_pass)}')
 
-        elif choice == "3": #Quit
+        elif choice == "3":  # Quit
             exit()
+
+
 main()
